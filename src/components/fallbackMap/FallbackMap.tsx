fee1232e3619376389ab1d26d9f234b715b9e1d3
hyprj <Krzysztofzur21@gmail.com>
2024-01-05 12:20:48 +0100

    fix env key name

--- src/components/fallbackMap/FallbackMap.tsx
+++ src/components/fallbackMap/FallbackMap.tsx
@@ -5,9 +5,9 @@ export function FallbackMap({ street, city }: { street: string; city: string }) 
       height="100%"
       loading="lazy"
       referrerpolicy="no-referrer-when-downgrade"
-      src={`https://www.google.com/maps/embed/v1/place?key=${import.meta.env.GOOGLE_MAPS_KEY}&q=${encodeURIComponent(
-        `${street}+${city}`,
-      )}`}
+      src={`https://www.google.com/maps/embed/v1/place?key=${
+        import.meta.env.VITE_GOOGLE_MAPS_KEY
+      }&q=${encodeURIComponent(`${street}+${city}`)}`}
     ></iframe>
   );
 }
